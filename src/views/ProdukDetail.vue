a8c6f3a6b060a391095b542e7c7d259890fc5ff1
Ahmad Wahana Jaya <119087167+Wannjayy@users.noreply.github.com>
2023-01-13 21:53:41 +0700

    1

--- src/views/ProdukDetail.vue
+++ src/views/ProdukDetail.vue
@@ -15,12 +15,26 @@
                 </div>
             </div>
 
-            <div class="row">
+            <div class="row  mt-3">
                 <div class="col md-6">
-                    <!--<img :src="'../assets/images'+product.gambar" class="img-fluid"/>-->
+                    <img :src="'../assets/images/'+product.gambar" class="img-fluid"/>
                 </div>
                 <div class="col md-6">
-                    <h2><strong>{{ $route.params.id }}</strong></h2>
+                    <h2><strong>{{ product.nama }}</strong></h2>
+                    <h4>Harga : <strong>Rp.{{ product.harga }}</strong></h4>
+                    <form class="mt-4" v-on:submit.prevent>
+                        <div class="col md-6">
+                            <div class="form-group">
+                                <label for="jumlah_pesanan">Jumlah Pesanan</label>
+                                <input type="number" class="form-control" v-model="pesan.jumlah_pesanan">
+                            </div>
+                            <div class="form-group">
+                                <label for="keterangan">Keterangan</label>
+                                <textarea v-model="pesan.keterangan" class="form-control" placeholder=""></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-success" @click="pesanan"><b-icon-cart></b-icon-cart>Beli</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -38,18 +52,28 @@ export default {
     },
     data() {
         return {
-            product: {}
+            product: {},
+            pesan: {}
         }
     },
     methods: {
         setProduct(data) {
             this.product = data
+        },
+        pesanan() {
+            this.pesan.product = this.product
+            axios
+             .post("https://api-wannjayy.vercel.app/keranjang", this.pesan)
+             .then(() => {
+                console.log("Berhasil")
+             })
+             .catch((err) => console.log(err))
         }
     },
     mounted() {
         axios
             .get("https://api-wannjayy.vercel.app/allproduk/"+this.$route.params.id)
-            .then((response) => this.setProducts(response.data))
+            .then((response) => this.setProduct(response.data))
             .catch((error) => console.log(error))
     }
 }
